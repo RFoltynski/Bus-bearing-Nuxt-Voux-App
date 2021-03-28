577dfa6f9ba57f79a1e4574a1d6677484236c25b
Rafal Foltynski <foltynski.rafal@gmail.com>
2021-03-28 11:20:52 +0200

    BB-4 Implementing basic components

---
+++ components/BusFilter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Bus Filter</h2>
+    <select>
+      <option></option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,15 @@
       <h1 class="title">
         bus-bearing
       </h1>
+      <BusFilter />
+      <BusLine />
     </div>
   </div>
 </template>
 
 <script>
+import BusLine from "../components/BusLine";
+import BusFilter from "../components/BusFilter";
 export default {};
 </script>
 
